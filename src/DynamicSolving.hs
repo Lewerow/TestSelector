b1e8bbb507e5cdcd526a510e067b1a028e5c0ddd
Lewerow <lewerow@wp.pl>
2014-04-28 23:00:06 +0200

    Some minor changes, it compiles, but I don't really think it works Need to get some tests, maybe tomorrow or on Wednesday I'll deploy HTF here

--- src/DynamicSolving.hs
+++ src/DynamicSolving.hs
@@ -18,8 +18,10 @@ module DynamicSolving (
 
 import Gradeable
 
+bestDynamicSolutions xs fs = sort $ solveDynamically xs fs
+
 solveDynamically:: Gradeable b => [[a]] -> [(a -> b)]  -> [[a]]
-solveDynamically xs fs = foldr mutator zip(xs, fs) [] where
+solveDynamically xs fs = foldr mutator (zip xs fs) [] where
   mutator acc (x, f) = f acc x
 
 -- to wszystko to będzie jeden foldr, tylko trzeba wymyślić sensownie jaki
