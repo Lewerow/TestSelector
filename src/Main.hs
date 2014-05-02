ccb8bd2ec5a223be52cbb19f58f5c5b8cb328802
Lewerow <lewerow@wp.pl>
2014-05-02 21:11:35 +0200

    Wrote first version of dynamic solving engine. A little brute-force, but I suppose it's just like the algorithm itself, so it's ok. Next step - TestData datatype!

--- src/Main.hs
+++ src/Main.hs
@@ -1,49 +1,57 @@
-{-# LANGUAGE CPP, TemplateHaskell #-}
+{-# OPTIONS_GHC -F -pgmF htfpp #-}
 -----------------------------------------------------------------------------
 --
 -- Module      :  Main
--- Copyright   :  
+-- Copyright   :
 -- License     :  AllRightsReserved
 --
--- Maintainer  :  
--- Stability   :  
--- Portability :  
+-- Maintainer  :
+-- Stability   :
+-- Portability :
 --
 -- |
 --
 -----------------------------------------------------------------------------
-
 module Main (
     main
 ) where
 
-import Control.Monad (unless)
-import Data.List (stripPrefix)
-import System.Exit (exitFailure)
-import Test.QuickCheck.All (quickCheckAll)
+import DynamicSolving
+import Test.Framework
+import Data.Monoid
+import Test.QuickCheck
+import Data.List
 
--- Simple function to create a hello message.
-hello s = "Hello " ++ s
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Graded a b) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    return (Gr x y)
 
--- Tell QuickCheck that if you strip "Hello " from the start of
--- hello s you will be left with s (for any s).
-prop_hello s = stripPrefix "Hello " (hello s) == Just s
+instance Show (a -> b) where
+    show x = "Function"
 
--- Hello World
-exeMain = do
-    putStrLn (hello "World")
+prop_memptyIsGradeableIdentity grd = mempty `mappend` grd == grd `mappend` mempty &&
+  mempty `mappend` grd == grd
+    where types = grd:: Graded [Int] Int
 
--- Entry point for unit tests.
-testMain = do
-    allPass <- $quickCheckAll -- Run QuickCheck on all prop_ functions
-    unless allPass exitFailure
+prop_mappendIsGradableAssociativeOperator grd grd2 grd3 =
+  (grd `mappend` grd2) `mappend` grd3 == grd `mappend` (grd2 `mappend` grd3)
+    where types = (grd:: Graded [Int] Int, grd2 ::Graded [Int] Int, grd3:: Graded [Int] Int)
 
--- This is a clunky, but portable, way to use the same Main module file
--- for both an application and for unit tests.
--- MAIN_FUNCTION is preprocessor macro set to exeMain or testMain.
--- That way we can use the same file for both an application and for tests.
-#ifndef MAIN_FUNCTION
-#define MAIN_FUNCTION exeMain
-#endif
-main = MAIN_FUNCTION
+prop_gradeableIsProperlyOrdered grd grd2 grd3 = not $ grd > grd2 && grd2 > grd3 && grd3 > grd
+  where types = (grd:: Graded [Int] Int, grd2 ::Graded [Int] Int, grd3:: Graded [Int] Int)
 
+prop_bestSolutionIsFirstElementOfOrderedList elems = null elems ||
+  (gr $ bestSolution (\x -> length x) elems) == (length $ head $ sortBy (\x y -> if (length x) < (length y) then GT else LT) elems)
+    where types = elems::[[Int]]
+
+prop_twoStepsDynamicSolvingBehavesAsReferenceImplementation grader mutator options = null options || length options > 5 ||
+  twoStepsBest grader mutator options == referenceTwoStepsBest grader mutator options
+    where types = (options::[[Int]], grader::[Int] -> Int, mutator:: [Int] -> [Int] -> [Int])
+
+
+
+prn = solveDynamically maximum (\_ y -> if length y == 0 then [] else tail y) [[1,22],[1,2,3],[42,1]] 2
+--main = htfMain htf_Main_thisModulesTests
+main = putStrLn $ show prn
