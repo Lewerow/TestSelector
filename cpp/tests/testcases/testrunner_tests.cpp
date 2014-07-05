c8c24a36a6a5e94dbfe9ac919879de95e6b46d4c
Tomasz Lewowski <tomasz.lewowski@wp.eu>
2014-07-05 22:18:43 +0200

    Added support for regex-based filtering of tests

--- cpp/tests/testcases/testrunner_tests.cpp
+++ cpp/tests/testcases/testrunner_tests.cpp
@@ -1,11 +1,21 @@
 #include <boost/test/auto_unit_test.hpp>
 #include <test_runner.h>
+#include <testcase_chooser.h>
+
+#include <vector>
+#include <string>
+#include <regex>
 
 BOOST_AUTO_TEST_SUITE(test_runner_tests)
 
 BOOST_AUTO_TEST_CASE(testcase_chooser_chooses_testcases_matching_filter)
 {
+	coverage_generator::testcase_chooser chooser ({ "first_test", "second_test", "third_test", "xxx", "", "xxx_test" });
+    chooser.filter(std::regex(".*_test"));
 
+	auto chosen = chooser.names();
+	auto expected = { "first_test", "second_test", "third_test", "xxx_test" };
+	BOOST_CHECK_EQUAL_COLLECTIONS(chosen.begin(), chosen.end(), expected.begin(), expected.end());
 }
 
 
