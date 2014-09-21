c9fa4beb15bcc5726e4c9398bed7e58f5b7e228c
Tomasz Lewowski <tomasz.lewowski@wp.eu>
2014-09-21 19:54:20 +0200

    Added missing files

---
+++ cpp/production/include/metaprogramming/are_unique.hpp
@@ -0,0 +1,15 @@
+#ifndef ARE_UNIQUE_mfdosfio34jf8934fjiruegesjfopsdkcmlkdnvkjrevifiorfofiioj34f
+#define ARE_UNIQUE_mfdosfio34jf8934fjiruegesjfopsdkcmlkdnvkjrevifiorfofiioj34f
+
+#include <boost/mpl/set.hpp>
+
+namespace metaprogramming
+{
+    template <typename... types>
+    struct are_unique
+	{
+	    
+	};
+}
+
+#endif
